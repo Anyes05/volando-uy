ccd9a3ee0e01b8417f5192e93363f0740272ff6c
FacundoCurbelo17 <ffacundoccurbelo@gmail.com>
2025-08-16 13:40:09 -0300

    Agregado de Pasaje, Reserva, CompraPaquete, CompraComun, Usuario y Cliente

---
+++ src/logica/clase/CompraPaquete.java
@@ -0,0 +1,23 @@
+package logica.clase;
+
+import logica.DataTypes.DTCliente;
+import logica.DataTypes.DTFecha;
+
+public class CompraPaquete extends Reserva {
+    private DTFecha vencimiento;
+
+    public CompraPaquete(DTCliente cliente, DTFecha fechaReserva, int costoReserva, DTFecha vencimiento) {
+        super(cliente, fechaReserva, costoReserva);
+
+        this.vencimiento = vencimiento;
+    }
+    // getters y setters
+    public DTFecha getVencimiento() {
+        return vencimiento;
+    }
+
+    public void setVencimiento(DTFecha vencimiento) {
+        this.vencimiento = vencimiento;
+    }
+}
+
