423b2432290151d57944ecc726fd8e2a936ca8bd
Anyes05 <anita20182005@gmail.com>
2025-08-15 22:40:05 -0300

    Estructura basica con clases dentro

---
+++ src/logica/datatype/tipoAsiento.java
@@ -0,0 +1,4 @@
+package logica.datatype;
+
+public enum tipoAsiento {
+}
---
+++ src/presentacion/AltaUsuario.java
@@ -0,0 +1,8 @@
+package presentacion;
+
+import javax.swing.*;
+
+public class AltaUsuario {
+    private JPanel panel1;
+    private JCheckBox checkBox1;
+}
