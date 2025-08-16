538b18f85e9cab06be0e17f0eb6e5a9e24a43d75
AguB19 <agustinbraggio1521@gmail.com>
2025-08-16 14:55:04 -0300

    Aropuerto, Ciudad y Categoría

--- src/logica/clase/Categoria.java
+++ src/logica/clase/Categoria.java
@@ -1,4 +1,22 @@
 package logica.clase;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Categoria {
+    private String nombre;
+    private List<RutaVuelo> rutas;
+
+    public Categoria(String nombre){
+        this.nombre = nombre;
+        this.rutas = new ArrayList<>();
+    }
+
+    public String getNombre() { return nombre; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public List<RutaVuelo> getRutas() { return rutas; }
+
+    public void setRutas(List<RutaVuelo> rutas) { this.rutas = rutas; }
+
 }
--- src/logica/clase/Ciudad.java
+++ src/logica/clase/Ciudad.java
@@ -1,4 +1,27 @@
 package logica.clase;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ciudad {
+    private String nombre;
+    private String pais;
+    private List<Aeropuerto> aeropuertos;
+
+    public Ciudad(String nombre, String pais){
+        this.nombre = nombre;
+        this.pais = pais;
+        this.aeropuertos = new ArrayList<>();
+    }
+
+    public String getNombre() { return nombre; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getPais() { return pais; }
+
+    public void setPais(String pais) { this.pais = pais; }
+
+    public List<Aeropuerto> getAeropuertos() { return this.aeropuertos; }
+
+    public void setAeropuertos(List<Aeropuerto> aeropuertos) { this.aeropuertos = aeropuertos; }
 }
