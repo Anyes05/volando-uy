ed0c5b761c3421800791a95d4f14b2bdd7c4c209
avriluch <avruglio17@gmail.com>
2025-08-17 13:24:06 -0300

    Main para pruebas

---
+++ src/logica/clase/MainTest.java
@@ -0,0 +1,11 @@
+package logica.clase;
+
+public class MainTest {
+    public static void main(String[] args) {
+        Factory factory = new Factory();
+        IEstacionTrabajo estacion = factory.getEstacionTrabajo();
+
+        System.out.println(estacion.consultarUsuarios()); // deberia de mostrar null. OK
+        System.out.println(estacion.validarNombre("test")); // deberia de mostrar false. OK
+    }
+}
