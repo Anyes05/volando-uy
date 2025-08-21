1bc10311e5fd39e6b51b86f1448b1ffc6dd2ed3f
galpper <110417657+galpper@users.noreply.github.com>
2025-08-21 00:16:00 -0300

    Grandes cambios de diseño
    Se cambia todo el formato, se agrega un menu tool bar, se cambia todo a jpanel. Ya no se utiliza jinternalframe. Se realizan links de botones del menu

--- src/presentacion/Principal.java
+++ src/presentacion/Principal.java
@@ -1,12 +1,15 @@
 package presentacion;
 
-import com.toedter.calendar.JCalendar;
+import  com.toedter.calendar.JCalendar;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
 
 public class Principal {
     private JPanel mainPrincipal;
-    private JInternalFrame altaRutaVuelo;
+    private JPanel altaRutaVuelo;
     private JComboBox aerolinea;
     private JPanel fromAltaRuta;
     private JTextField nombre;
@@ -20,11 +23,11 @@ public class Principal {
     private JCalendar JCalendar1;
     private JTextArea textArea1;
     private JButton Enviar;
-    private JInternalFrame altaUsuario;
-    private JInternalFrame altaAerolinea;
-    private JButton crearNuevoClienteButton;
-    private JButton crearNuevaAerolineaButton;
-    private JInternalFrame altaCliente;
+    private JPanel altaUsuario;
+    private JPanel altaAerolinea;
+    private JButton ButtonCrearNuevoCliente;
+    private JButton ButtonCrearNuevaAerolinea;
+    private JPanel altaCliente;
     private JTextField Apellido;
     private JTextField Nombre;
     private JTextField Nickname;
@@ -34,7 +37,7 @@ public class Principal {
     private JButton cancelarButton;
     private JButton aceptarButton;
     private JTextPane textPane1;
-    private JInternalFrame consultaUsuario;
+    private JPanel consultaUsuario;
     private JTextField textField3;
     private JButton aceptarButton1;
     private JRadioButton usuarioRadioButton;
@@ -43,7 +46,7 @@ public class Principal {
     private JRadioButton paqueteVueloRadioButton;
     private JTextField textField4;
     private JButton confirmarButton;
-    private JInternalFrame modificarUsuario;
+    private JPanel modificarUsuario;
     private JTextField textField5;
     private JButton aceptarButton2;
     private JTextField textField6;
@@ -53,6 +56,90 @@ public class Principal {
     private JTextField textField10;
     private JTextArea textArea2;
     private JButton guardarButton;
+    private JPanel parentPanel;
+    private JPanel buttonsPanel;
+    private JButton botonInicio;
+    private JComboBox comboBoxVuelos;
+    private JComboBox comboBoxUsuario;
+    private JPanel panelVacio;
+    private JToolBar JToolBarPrincipal;
+    private JButton button2;
+
+    public Principal() {
+        botonInicio.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                parentPanel.removeAll();
+                parentPanel.add(panelVacio);
+                parentPanel.repaint();
+                parentPanel.revalidate();
+            }
+        });
+
+        comboBoxVuelos.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String seleccionado = (String) comboBoxVuelos.getSelectedItem();
+                // Según lo que se elija, haces algo
+                switch (seleccionado) {
+                    case "Crear ruta de vuelo":
+                        parentPanel.removeAll();
+                        parentPanel.add(altaRutaVuelo);
+                        parentPanel.repaint();
+                        parentPanel.revalidate();
+                        break;
+                }
+            }
+        });
+
+        comboBoxUsuario.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String seleccionado = (String) comboBoxUsuario.getSelectedItem();
+                // Según lo que se elija, haces algo
+                switch (seleccionado) {
+                    case "Crear usuario":
+                        parentPanel.removeAll();
+                        parentPanel.add(altaUsuario);
+                        parentPanel.repaint();
+                        parentPanel.revalidate();
+                        break;
+                    case "Modificar usuario":
+                        parentPanel.removeAll();
+                        parentPanel.add(modificarUsuario);
+                        parentPanel.repaint();
+                        parentPanel.revalidate();
+                        break;
+                    case "Consultar usuario":
+                        parentPanel.removeAll();
+                        parentPanel.add(consultaUsuario);
+                        parentPanel.repaint();
+                        parentPanel.revalidate();
+                        break;
+                }
+                }
+        });
+        ButtonCrearNuevoCliente.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                parentPanel.removeAll();
+                parentPanel.add(altaCliente);
+                parentPanel.repaint();
+                parentPanel.revalidate();
+            }
+        });
+        ButtonCrearNuevaAerolinea.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                parentPanel.removeAll();
+                parentPanel.add(altaAerolinea);
+                parentPanel.repaint();
+                parentPanel.revalidate();
+            }
+        });
+        panelVacio.addComponentListener(new ComponentAdapter() {
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Principal");
