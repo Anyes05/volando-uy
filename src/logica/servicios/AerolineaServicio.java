a713d7da7e0863adcb1a4b7d8eaa2cdc1e74349e
avriluch <avruglio17@gmail.com>
2025-08-30 17:02:42 -0300

    alta vuelo y consultar vuelo jpa

--- src/logica/servicios/AerolineaServicio.java
+++ src/logica/servicios/AerolineaServicio.java
@@ -2,7 +2,12 @@ package logica.servicios;
 
 import dato.dao.AerolineaDAO;
 import dato.entidades.Aerolinea;
+import logica.DataTypes.DTRutaVuelo;
+import dato.entidades.RutaVuelo;
+import logica.DataTypes.DTAerolinea;
+import logica.DataTypes.DTCiudad;
 import java.util.List;
+import java.util.ArrayList;
 
 public class AerolineaServicio {
     private AerolineaDAO aerolineaDAO;
@@ -39,5 +44,26 @@ public class AerolineaServicio {
 
     public void eliminarAerolinea(Long id) {
         aerolineaDAO.eliminar(id);
+    }
+
+    // AerolineaServicio.java
+    public List<DTRutaVuelo> obtenerRutasDeAerolinea(String nickname) {
+        Aerolinea aerolinea = buscarAerolineaPorNickname(nickname);
+        if (aerolinea == null) {
+            throw new IllegalStateException("No se encontró una aerolínea con el nickname: " + nickname);
+        }
+        List<DTRutaVuelo> listaRutas = new ArrayList<>();
+        for (RutaVuelo r : aerolinea.getRutasVuelo()) {
+            listaRutas.add(new DTRutaVuelo(
+                    r.getNombre(),
+                    r.getDescripcion(),
+                    r.getFechaAlta(),
+                    r.getCostoBase(),
+                    new DTAerolinea(aerolinea.getNickname(), aerolinea.getNombre(), aerolinea.getCorreo(), aerolinea.getDescripcion(), aerolinea.getLinkSitioWeb(), new ArrayList<>()),
+                    new DTCiudad(r.getCiudadOrigen().getNombre(), r.getCiudadOrigen().getPais()),
+                    new DTCiudad(r.getCiudadDestino().getNombre(), r.getCiudadDestino().getPais())
+            ));
+        }
+        return listaRutas;
     }
 }
