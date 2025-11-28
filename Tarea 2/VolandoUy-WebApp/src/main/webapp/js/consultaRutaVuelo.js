b3810164ea1642cb38beaaff30d19e103684be43
FacundoCurbelo17 <ffacundoccurbelo@gmail.com>
2025-11-27 21:25:25 -0300

    Revert "x2"
    This reverts commit 4b3024073881da86d96735ebf9d06efae3e4214c.

--- Tarea 2/VolandoUy-WebApp/src/main/webapp/js/consultaRutaVuelo.js
+++ Tarea 2/VolandoUy-WebApp/src/main/webapp/js/consultaRutaVuelo.js
@@ -2,12 +2,7 @@
 // Helper para construir URLs con contextPath
 function apiUrl(path) {
   const contextPath = window.APP_CONTEXT_PATH || '';
-  const url = contextPath + (path.startsWith('/') ? path : '/' + path);
-  // Debug: solo mostrar si hay un problema
-  if (!contextPath) {
-    console.warn('APP_CONTEXT_PATH no está definido. URL generada:', url);
-  }
-  return url;
+  return contextPath + (path.startsWith('/') ? path : '/' + path);
 }
 
 let rutasData = [];
